8af20c61d1cedcf57463952f97b4d12dc7d72de7
Dina-Eleiba <dina.adel@outlook.com>
2024-08-07 07:03:09 -0700

    ggg

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,48 +1,34 @@
 import React from "react";
+import "./CourseList.css";
+import CourseListRow from "./CourseListRow";
 import PropTypes from "prop-types";
+import CourseShape from "./CourseShape";
 
-const rowStyle = {
-  backgroundColor: "#f5f5f5ab",
-};
-
-const headerStyle = {
-  backgroundColor: "#deb5b545",
-};
-
-function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+function CourseList({ listCourses }) {
   return (
-    <tr style={rowStyle}>
-      {isHeader ? (
-        textSecondCell === null ? (
-          <th style={headerStyle} colSpan={2}>
-            {textFirstCell}
-          </th>
+    <table id="CourseList">
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
         ) : (
-          <>
-            <th style={headerStyle}>{textFirstCell}</th>
-            <th style={headerStyle}>{textSecondCell}</th>
-          </>
-        )
-      ) : (
-        <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
-        </>
-      )}
-    </tr>
+          <CourseListRow textFirstCell="No course available yet" />
+        )}
+      </tbody>
+    </table>
   );
 }
 
-CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
 };
 
-CourseListRow.defaultProps = {
-  isHeader: false,
-  textSecondCell: null,
+CourseList.defaultProps = {
+  listCourses: [],
 };
 
-export default CourseListRow;
+export default CourseList;
 
